d65b9e7e6e6e21aafb29e8823f1c1de6ea79ac37
Damon Young <damonyoungcc@icloud.com>
2023-08-09 21:50:24 +0800

    中文简体文档 (#1)
    * add zh-CN config
    
    * translate d3-scale
    
    * feat: scale
    
    * feat: translate home page
    
    * feat: d3-array 求和
    
    * feat: d3 bin
    
    * feat: update config
    
    * fix: component import path
    
    * feat: 随机数
    
    * translate What is D3
    
    * feat: d3-random translate
    
    * feat: d3.random translate
    
    * translate d3.randomIrwinHall

---
+++ docs/.vitepress/config/zh-CN.ts
@@ -0,0 +1,350 @@
+import { fileURLToPath, URL } from "node:url";
+import path from "node:path";
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "D3 by Observable",
+  description: "可实现定制数据可视化的JavaScript库",
+  cleanUrls: true,
+  sitemap: {
+    hostname: "https://d3js.org"
+  },
+  head: [
+    ["link", { rel: "apple-touch-icon", href: "/logo.png" }],
+    ["link", { rel: "icon", type: "image/png", href: "/logo.png" }],
+  ],
+  markdown: {
+    externalLinks: {
+      rel: "external"
+    }
+  },
+  vite: {
+    resolve: {
+      alias: [
+        { find: "d3", replacement: path.resolve("./dist/d3.mjs") },
+        {
+          find: /^.*\/VPFooter\.vue$/,
+          replacement: fileURLToPath(
+            new URL("./theme/CustomFooter.vue", import.meta.url)
+          ),
+        },
+      ],
+    }
+  },
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: "/logo.svg",
+    siteTitle: "",
+    nav: [
+      { text: "首页", link: "/zh-CN/" },
+      { text: "文档", link: "/zh-CN/what-is-d3" },
+      {
+        text: "示例",
+        link: "https://observablehq.com/@d3/gallery?utm_source=d3js-org&utm_medium=nav&utm_campaign=try-observable",
+        rel: "external"
+      },
+      { text: "社区", link: "/zh-CN/community" },
+      {
+        text: "Plot",
+        link: "https://observablehq.com/plot?utm_source=d3js-org&utm_medium=nav&utm_campaign=try-observable",
+        rel: "external"
+      },
+    ],
+    sidebar: [
+      {
+        text: "介绍",
+        items: [
+          { text: "D3是什么?", link: "/zh-CN/what-is-d3" },
+          { text: "快速开始", link: "/zh-CN/getting-started" },
+        ],
+      },
+      {
+        text: "可视化 Visualization",
+        items: [
+          { text: "d3-axis", link: "/zh-CN/d3-axis" },
+          {
+            text: "d3-chord",
+            link: "/zh-CN/d3-chord",
+            collapsed: true,
+            items: [
+              { text: "Chords", link: "/zh-CN/d3-chord/chord" },
+              { text: "Ribbons", link: "/zh-CN/d3-chord/ribbon" },
+            ],
+          },
+          { text: "d3-color", link: "/zh-CN/d3-color" },
+          {
+            text: "d3-interpolate",
+            link: "/zh-CN/d3-interpolate",
+            collapsed: true,
+            items: [
+              {
+                text: "Value interpolation",
+                link: "/zh-CN/d3-interpolate/value",
+              },
+              {
+                text: "Color interpolation",
+                link: "/zh-CN/d3-interpolate/color",
+              },
+              {
+                text: "Transform interpolation",
+                link: "/zh-CN/d3-interpolate/transform",
+              },
+              {
+                text: "Zoom interpolation",
+                link: "/zh-CN/d3-interpolate/zoom",
+              },
+            ],
+          },
+          {
+            text: "d3-contour",
+            link: "/zh-CN/d3-contour",
+            collapsed: true,
+            items: [
+              { text: "Contour polygons", link: "/zh-CN/d3-contour/contour" },
+              { text: "Density estimation", link: "/zh-CN/d3-contour/density" },
+            ],
+          },
+          {
+            text: "d3-delaunay",
+            link: "/zh-CN/d3-delaunay",
+            collapsed: true,
+            items: [
+              {
+                text: "Delaunay triangulations",
+                link: "/zh-CN/d3-delaunay/delaunay",
+              },
+              { text: "Voronoi diagrams", link: "/zh-CN/d3-delaunay/voronoi" },
+            ],
+          },
+          {
+            text: "d3-force",
+            link: "/zh-CN/d3-force",
+            collapsed: true,
+            items: [
+              { text: "Force simulations", link: "/zh-CN/d3-force/simulation" },
+              { text: "Center force", link: "/zh-CN/d3-force/center" },
+              { text: "Collide force", link: "/zh-CN/d3-force/collide" },
+              { text: "Link force", link: "/zh-CN/d3-force/link" },
+              { text: "Many-body force", link: "/zh-CN/d3-force/many-body" },
+              { text: "Position forces", link: "/zh-CN/d3-force/position" },
+            ],
+          },
+          {
+            text: "d3-geo",
+            link: "/zh-CN/d3-geo",
+            collapsed: true,
+            items: [
+              { text: "Paths", link: "/zh-CN/d3-geo/path" },
+              {
+                text: "Projections",
+                link: "/zh-CN/d3-geo/projection",
+                collapsed: true,
+                items: [
+                  {
+                    text: "Azimuthal projections",
+                    link: "/zh-CN/d3-geo/azimuthal",
+                  },
+                  { text: "Conic projections", link: "/zh-CN/d3-geo/conic" },
+                  {
+                    text: "Cylindrical projections",
+                    link: "/zh-CN/d3-geo/cylindrical",
+                  },
+                ],
+              },
+              { text: "Streams", link: "/zh-CN/d3-geo/stream" },
+              { text: "Spherical shapes", link: "/zh-CN/d3-geo/shape" },
+              { text: "Spherical math", link: "/zh-CN/d3-geo/math" },
+            ],
+          },
+          {
+            text: "d3-hierarchy",
+            link: "/zh-CN/d3-hierarchy",
+            collapsed: true,
+            items: [
+              { text: "Hierarchies", link: "/zh-CN/d3-hierarchy/hierarchy" },
+              { text: "Stratify", link: "/zh-CN/d3-hierarchy/stratify" },
+              { text: "Tree", link: "/zh-CN/d3-hierarchy/tree" },
+              { text: "Cluster", link: "/zh-CN/d3-hierarchy/cluster" },
+              { text: "Partition", link: "/zh-CN/d3-hierarchy/partition" },
+              { text: "Pack", link: "/zh-CN/d3-hierarchy/pack" },
+              { text: "Treemap", link: "/zh-CN/d3-hierarchy/treemap" },
+            ],
+          },
+          { text: "d3-path", link: "/zh-CN/d3-path" },
+          { text: "d3-polygon", link: "/zh-CN/d3-polygon" },
+          { text: "d3-quadtree", link: "/zh-CN/d3-quadtree" },
+          {
+            text: "比例尺 d3-scale",
+            link: "/zh-CN/d3-scale",
+            collapsed: true,
+            items: [
+              { text: "线性比例尺 Linear scale", link: "/zh-CN/d3-scale/linear" },
+              { text: "Time scales", link: "/zh-CN/d3-scale/time" },
+              { text: "Pow scales", link: "/zh-CN/d3-scale/pow" },
+              { text: "Log scales", link: "/zh-CN/d3-scale/log" },
+              { text: "Symlog scales", link: "/zh-CN/d3-scale/symlog" },
+              { text: "Ordinal scales", link: "/zh-CN/d3-scale/ordinal" },
+              { text: "Band scales", link: "/zh-CN/d3-scale/band" },
+              { text: "Point scales", link: "/zh-CN/d3-scale/point" },
+              { text: "Sequential scales", link: "/zh-CN/d3-scale/sequential" },
+              { text: "Diverging scales", link: "/zh-CN/d3-scale/diverging" },
+              { text: "Quantile scales", link: "/zh-CN/d3-scale/quantile" },
+              { text: "Quantize scales", link: "/zh-CN/d3-scale/quantize" },
+              { text: "Threshold scales", link: "/zh-CN/d3-scale/threshold" },
+            ],
+          },
+          {
+            text: "d3-scale-chromatic",
+            link: "/zh-CN/d3-scale-chromatic",
+            collapsed: true,
+            items: [
+              {
+                text: "Categorical schemes",
+                link: "/zh-CN/d3-scale-chromatic/categorical",
+              },
+              {
+                text: "Cyclical schemes",
+                link: "/zh-CN/d3-scale-chromatic/cyclical",
+              },
+              {
+                text: "Diverging schemes",
+                link: "/zh-CN/d3-scale-chromatic/diverging",
+              },
+              {
+                text: "Sequential schemes",
+                link: "/zh-CN/d3-scale-chromatic/sequential",
+              },
+            ],
+          },
+          {
+            text: "d3-selection",
+            link: "/zh-CN/d3-selection",
+            collapsed: true,
+            items: [
+              {
+                text: "Selecting elements",
+                link: "/zh-CN/d3-selection/selecting",
+              },
+              {
+                text: "Modifying elements",
+                link: "/zh-CN/d3-selection/modifying",
+              },
+              { text: "Joining data", link: "/zh-CN/d3-selection/joining" },
+              { text: "Handling events", link: "/zh-CN/d3-selection/events" },
+              {
+                text: "Control flow",
+                link: "/zh-CN/d3-selection/control-flow",
+              },
+              { text: "Local variables", link: "/zh-CN/d3-selection/locals" },
+              { text: "Namespaces", link: "/zh-CN/d3-selection/namespaces" },
+            ],
+          },
+          {
+            text: "d3-shape",
+            link: "/zh-CN/d3-shape",
+            collapsed: true,
+            items: [
+              { text: "Arcs", link: "/zh-CN/d3-shape/arc" },
+              { text: "Areas", link: "/zh-CN/d3-shape/area" },
+              { text: "Curves", link: "/zh-CN/d3-shape/curve" },
+              { text: "Lines", link: "/zh-CN/d3-shape/line" },
+              { text: "Links", link: "/zh-CN/d3-shape/link" },
+              { text: "Pies", link: "/zh-CN/d3-shape/pie" },
+              { text: "Stacks", link: "/zh-CN/d3-shape/stack" },
+              { text: "Symbols", link: "/zh-CN/d3-shape/symbol" },
+              { text: "Radial areas", link: "/zh-CN/d3-shape/radial-area" },
+              { text: "Radial lines", link: "/zh-CN/d3-shape/radial-line" },
+              { text: "Radial links", link: "/zh-CN/d3-shape/radial-link" },
+            ],
+          },
+        ],
+      },
+      {
+        text: "动画 Animation",
+        items: [
+          { text: "d3-ease", link: "/zh-CN/d3-ease" },
+          { text: "d3-timer", link: "/zh-CN/d3-timer" },
+          {
+            text: "d3-transition",
+            link: "/zh-CN/d3-transition",
+            collapsed: true,
+            items: [
+              {
+                text: "Selecting elements",
+                link: "/zh-CN/d3-transition/selecting",
+              },
+              {
+                text: "Modifying elements",
+                link: "/zh-CN/d3-transition/modifying",
+              },
+              { text: "Timing", link: "/zh-CN/d3-transition/timing" },
+              {
+                text: "Control flow",
+                link: "/zh-CN/d3-transition/control-flow",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        text: "交互式 Interaction",
+        items: [
+          { text: "d3-brush", link: "/zh-CN/d3-brush" },
+          { text: "d3-dispatch", link: "/zh-CN/d3-dispatch" },
+          { text: "d3-drag", link: "/zh-CN/d3-drag" },
+          { text: "d3-zoom", link: "/zh-CN/d3-zoom" },
+        ],
+      },
+      {
+        text: "数据 Data",
+        items: [
+          {
+            text: "数组 d3-array",
+            link: "/zh-CN/d3-array",
+            collapsed: true,
+            items: [
+              { text: "数值求和 Adding numbers", link: "/zh-CN/d3-array/add" },
+              { text: "Binning data", link: "/zh-CN/d3-array/bin" },
+              { text: "Bisecting data", link: "/zh-CN/d3-array/bisect" },
+              { text: "Blurring data", link: "/zh-CN/d3-array/blur" },
+              { text: "Grouping data", link: "/zh-CN/d3-array/group" },
+              { text: "Interning values", link: "/zh-CN/d3-array/intern" },
+              { text: "Set operations", link: "/zh-CN/d3-array/sets" },
+              { text: "Sorting data", link: "/zh-CN/d3-array/sort" },
+              { text: "Summarizing data", link: "/zh-CN/d3-array/summarize" },
+              { text: "Ticks", link: "/zh-CN/d3-array/ticks" },
+              { text: "Transforming data", link: "/zh-CN/d3-array/transform" },
+            ],
+          },
+          { text: "d3-dsv", link: "/zh-CN/d3-dsv" },
+          { text: "d3-fetch", link: "/zh-CN/d3-fetch" },
+          { text: "d3-format", link: "/zh-CN/d3-format" },
+          { text: "随机数 d3-random", link: "/zh-CN/d3-random" },
+          { text: "d3-time", link: "/zh-CN/d3-time" },
+          { text: "d3-time-format", link: "/zh-CN/d3-time-format" },
+        ],
+      },
+      { text: "API index", link: "/zh-CN/api" },
+    ],
+    search: {
+      provider: "local",
+    },
+    socialLinks: [
+      { icon: "github", link: "https://github.com/d3" },
+      { icon: "twitter", link: "https://twitter.com/observablehq" },
+      { icon: "mastodon", link: "https://vis.social/@observablehq" },
+      {
+        icon: "slack",
+        link: "https://join.slack.com/t/observable-community/shared_invite/zt-1x7gs4fck-UHhEFxUXKHVE8Qt3XmJCig",
+      },
+      { icon: "linkedin", link: "https://www.linkedin.com/company/observable" },
+      { icon: "youtube", link: "https://www.youtube.com/c/Observablehq" },
+    ],
+    footer: {
+      message:
+        "Library released under <a style='text-decoration:underline;' href='https://github.com/d3/d3/blob/main/LICENSE'>ISC License</a>.",
+      copyright: `Copyright 2010–${new Date().getUTCFullYear()} Mike Bostock`,
+    },
+  },
+});
